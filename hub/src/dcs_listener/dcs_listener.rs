b165f8556eea66d428b9d291dd97163406b86625
daxtonb <daxton@smartxadvisory.com>
2024-01-28 23:23:14 -0500

    Refactor

--- hub/src/dcs_listener/dcs_listener.rs
+++ hub/src/dcs_listener/dcs_listener.rs
@@ -5,14 +5,15 @@ use super::dcs_unit::DcsUnit;
 
 pub const DCS_LISTENER_PORT: u16 = 34254;
 pub const DCS_MSG_DELIMITER: u8 = b'\n';
+pub const DCS_LISTENER_BUFFER_SIZE: usize = 1024;
 
 /// Starts a thread that will continuously listen for the DCS units export.
-/// 
+///
 /// # Arguments
 /// * `units_handler` - Closure for handling any captured DCS units from the export.
 pub async fn listen<F>(units_handler: F) -> Result<(), Box<dyn Error>>
 where
-    F: Fn(Vec<DcsUnit>) + Send + Sync + 'static,
+    F: Fn(DcsUnit) + Send + Sync + 'static,
 {
     let socket = setup_socket().await?;
     start_receiving_loop(socket, units_handler).await;
@@ -20,16 +21,16 @@ where
     Ok(())
 }
 
-async fn start_receiving_loop<F>(socket: UdpSocket, units_handler: F) -> JoinHandle<()>
+async fn start_receiving_loop<F>(socket: UdpSocket, unit_handler: F) -> JoinHandle<()>
 where
-    F: Fn(Vec<DcsUnit>) + Send + Sync + 'static,
+    F: Fn(DcsUnit) + Send + Sync + 'static,
 {
-    let buffer = [0u8; 1024];
+    let buffer = [0u8; DCS_LISTENER_BUFFER_SIZE];
 
     tokio::spawn(async move {
         loop {
             match receive_next(&socket, buffer).await {
-                Ok(units) => units_handler(units),
+                Ok(unit) => unit_handler(unit),
                 Err(e) => {
                     println!("Error receiving message: {}", e);
                     break;
@@ -48,22 +49,22 @@ async fn setup_socket() -> Result<UdpSocket, Box<dyn Error>> {
 async fn receive_next(
     socket: &UdpSocket,
     mut buffer: [u8; 1024],
-) -> Result<Vec<DcsUnit>, Box<dyn Error>> {
-    let mut current_msg = Vec::new();
+) -> Result<DcsUnit, Box<dyn Error>> {
+    let size = socket.recv(&mut buffer).await?;
 
-    loop {
-        let size = socket.recv(&mut buffer).await?;
+    if size == 0 {
+        // No data received, possibly due to a closed connection or other issue
+        return Err("No data received".into());
+    }
 
-        if let Some(position) = buffer[..size].iter().position(|&b| b == DCS_MSG_DELIMITER) {
-            current_msg.extend_from_slice(&buffer[..position]);
-            let msg = String::from_utf8_lossy(&current_msg);
-            let units = serde_json::from_str::<Vec<DcsUnit>>(&msg)?;
-            current_msg.clear();
-
-            return Ok(units);
-        } else {
-            current_msg.extend_from_slice(&buffer[..size]);
-        }
+    // Find the message delimiter in the datagram, then return the deserialized result
+    if let Some(position) = buffer[..size].iter().position(|&b| b == DCS_MSG_DELIMITER) {
+        let msg = String::from_utf8_lossy(&buffer[..position]);
+        let unit = serde_json::from_str::<DcsUnit>(&msg)?;
+        Ok(unit)
+    } else {
+        // Delimiter not found in the current datagram
+        Err("Message delimiter not found in the datagram".into())
     }
 }
 
@@ -78,27 +79,32 @@ mod integration_tests {
 
     #[tokio::test]
     async fn test_listen() {
-        let units = vec![DcsUnit {}, DcsUnit {}];
-        let mut serialized_units = serde_json::to_string(&units).unwrap();
-        serialized_units.push(DCS_MSG_DELIMITER as char);
+        // Create a few units
+        let mut units = Vec::new();
+        for _ in 0..3 {
+            units.push(DcsUnit {
+                group_name: String::from("test"),
+            });
+        }
 
-        let units_handler = move |received_units: Vec<DcsUnit>| {
-            assert_eq!(received_units.len(), units.len());
-
-            for received_unit in received_units {
-                assert!(&units.contains(&received_unit))
+        // Create a channel to signal that units_handler was called
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let unit_handler = {
+            let tx = tx.clone();
+            move |received_unit: DcsUnit| {
+                let tx = tx.clone();
+                tokio::spawn(async move {
+                    tx.send(received_unit).await.expect("Failed to send signal");
+                });
             }
         };
 
         // Start the listener
         let list_task = tokio::spawn(async move {
-            listen(units_handler)
+            listen(unit_handler)
                 .await
                 .expect("Unable to start listener")
         });
-
-        // Give the listener some time to set up
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
 
         // Create a separate socket for sending messages
         let sender_socket = UdpSocket::bind("127.0.0.1:0") // Bind to an arbitrary available port
@@ -109,14 +115,29 @@ mod integration_tests {
             .await
             .expect("Unable to connect to listener");
 
-        // Send the message
-        sender_socket
-            .send(serialized_units.as_bytes())
-            .await
-            .expect("Unable to send message");
+        // Send units to the listener via socket
+        for unit in &units {
+            let mut serialized_unit = serde_json::to_string(&unit).unwrap();
+            serialized_unit.push(DCS_MSG_DELIMITER as char);
 
-        // Wait for a bit to ensure the message is processed
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+            sender_socket
+                .send(serialized_unit.as_bytes())
+                .await
+                .expect("Unable to send message");
+        }
+
+        // Check if all units were received
+        let mut units_count = 0;
+        while let Some(unit) = rx.recv().await {
+            assert!(units.contains(&unit));
+            units_count += 1;
+
+            if units_count == units.len() {
+                break;
+            }
+        }
+
+        assert_eq!(units_count, units.len());
 
         list_task.await.unwrap();
     }
--- hub/src/dcs_listener/dcs_unit.rs
+++ hub/src/dcs_listener/dcs_unit.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
-pub struct DcsUnit {} 
+pub struct DcsUnit {
+    pub group_name: String
+} 
