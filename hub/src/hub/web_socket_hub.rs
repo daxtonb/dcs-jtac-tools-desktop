bd1a20228b14c875938f5998838dbea2bb27f3e9
daxtonb <daxtonburgon@gmail.com>
2024-03-12 23:22:47 -0400

    More refactoring

--- hub/src/hub/web_socket_hub.rs
+++ hub/src/hub/web_socket_hub.rs
@@ -48,15 +48,20 @@ impl WebSocketHub {
 
             let clients_by_id_clone = self.clients_by_id.clone();
             let next_client_id_clone = self.next_client_id.clone();
-            // Spawn a task for handling the new client, passing only the necessary parts of `self`.
+
+            // Spawn a task for handling and tracking the new client
             tokio::spawn(async move {
-                Self::handle_new_client(clients_by_id_clone, ws_stream, next_client_id_clone);
+                let client_id =
+                    Self::handle_new_client(&clients_by_id_clone, ws_stream, &next_client_id_clone)
+                        .await;
+                Self::listen_to_client(&clients_by_id_clone, &client_id).await;
+                Self::handle_client_disconnected(&clients_by_id_clone, &client_id).await;
             });
         }
 
         Ok(())
-    } 
-    
+    }
+
     /// Sends a message to all subscribers.
     pub async fn broadcast_message(&self, message: String) -> Result<(), Error> {
         let clients = self.clients_by_id.lock().await;
@@ -68,6 +73,7 @@ impl WebSocketHub {
                 let message = message.clone();
                 let client = client.clone();
                 async move {
+                    // TODO: Explore the possibility of a more granular/semantic lock
                     let mut client = client.lock().await;
 
                     // Messages are not anticipated to be large
@@ -85,10 +91,10 @@ impl WebSocketHub {
     }
 
     async fn handle_new_client(
-        clients_by_id: ClientsById,
+        clients_by_id: &ClientsById,
         ws_stream: WebSocketStream<TcpStream>,
-        next_client_id: Arc<AtomicU32>,
-    ) {
+        next_client_id: &Arc<AtomicU32>,
+    ) -> u32 {
         let client_id = next_client_id.fetch_add(1, Ordering::Relaxed);
         let client_arc = Arc::new(Mutex::new(ws_stream));
         let client_arc_clone = client_arc.clone();
@@ -99,39 +105,36 @@ impl WebSocketHub {
         }
 
         println!("New client: {}", client_id);
-        let ws_stream = client_arc.lock().await;
-
-        Self::listen_to_client(ws_stream, client_id, clients_by_id).await;
+        return client_id;
     }
 
-    async fn listen_to_client(
-        mut ws_stream: futures_util::lock::MutexGuard<'_, WebSocketStream<TcpStream>>,
-        client_id: u32,
-        clients_by_id: Arc<Mutex<HashMap<u32, Arc<Mutex<WebSocketStream<TcpStream>>>>>>,
-    ) {
-        // Here we're just looping to detect disconnection.
-        while let Some(result) = ws_stream.next().await {
-            match result {
-                Ok(_) => {
-                    // TODO: handle incoming client messages
-                }
-                Err(e) => {
-                    eprintln!("Error on WebSocket for client {}: {:?}", client_id, e);
-                    break; // Exit gracefully
+    async fn listen_to_client(clients_by_id: &ClientsById, client_id: &u32) {
+        let client = clients_by_id.lock().await.get(client_id).cloned();
+        match client {
+            Some(client) => {
+                // Here we're just looping to detect disconnection.
+                while let Some(result) = client.lock().await.next().await {
+                    match result {
+                        Ok(_) => {
+                            // TODO: handle incoming client messages
+                        }
+                        Err(e) => {
+                            eprintln!("Error on WebSocket for client {:?}: {:?}", client_id, e);
+                            break; // Exit gracefully
+                        }
+                    }
                 }
             }
+            None => eprintln!(
+                "Something went wrong while trying to listen to client {}",
+                client_id
+            ),
         }
-
-        // If we get here, the client has disconnected
-        Self::handle_client_disconnected(clients_by_id, client_id).await;
     }
 
-    async fn handle_client_disconnected(
-        clients_by_id: Arc<Mutex<HashMap<u32, Arc<Mutex<WebSocketStream<TcpStream>>>>>>,
-        client_id: u32,
-    ) {
+    async fn handle_client_disconnected(clients_by_id: &ClientsById, client_id: &u32) {
         let mut clients_lock = clients_by_id.lock().await;
-        clients_lock.remove(&client_id);
+        clients_lock.remove(client_id);
         println!("Client {} disconnected", client_id);
     }
 }
