d4c68fd05534c214892d0cfd57dd1134028fd93c
daxtonb <daxtonburgon@gmail.com>
2024-03-17 01:19:47 -0400

    Completed web socket hub

--- hub/src/hub/web_socket_hub.rs
+++ hub/src/hub/web_socket_hub.rs
@@ -6,20 +6,30 @@ use std::{
     },
 };
 
-use futures_util::{future::join_all, lock::Mutex, SinkExt, StreamExt};
+use futures_util::{
+    future::join_all,
+    lock::Mutex,
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{
     accept_async,
     tungstenite::{Error, Message},
-    WebSocketStream,
+    MaybeTlsStream, WebSocketStream,
 };
 
-type Client = Arc<Mutex<WebSocketStream<TcpStream>>>;
-type ClientsById = Arc<Mutex<HashMap<u32, Client>>>;
+type ReadHalf = SplitStream<WebSocketStream<TcpStream>>;
+type WriteHalf = SplitSink<WebSocketStream<TcpStream>, Message>;
+type ClientRead = Arc<Mutex<ReadHalf>>;
+type ClientWrite = Arc<Mutex<WriteHalf>>;
+type ClientsByIdRead = Arc<Mutex<HashMap<u32, ClientRead>>>;
+type ClientsByIdWrite = Arc<Mutex<HashMap<u32, ClientWrite>>>;
 
 /// Hub for managing web socket communication.
 pub struct WebSocketHub {
-    clients_by_id: ClientsById,
+    clients_by_id_read: ClientsByIdRead,
+    clients_by_id_write: ClientsByIdWrite,
     port: u16,
     next_client_id: Arc<AtomicU32>,
 }
@@ -28,7 +38,8 @@ impl WebSocketHub {
     /// Instantiates a new `WebSocketHub` for a port.
     pub fn new(port: u16) -> WebSocketHub {
         WebSocketHub {
-            clients_by_id: ClientsById::default(),
+            clients_by_id_read: ClientsByIdRead::default(),
+            clients_by_id_write: ClientsByIdWrite::default(),
             port: port,
             next_client_id: Arc::new(AtomicU32::new(0)),
         }
@@ -47,17 +58,30 @@ impl WebSocketHub {
                 }
             };
 
-            let clients_by_id_clone = self.clients_by_id.clone();
+            let clients_by_id_read = self.clients_by_id_read.clone();
+            let clients_by_id_write = self.clients_by_id_write.clone();
             let next_client_id_clone = self.next_client_id.clone();
-            let client = Arc::new(Mutex::new(ws_stream));
+            let (write_half, read_half) = ws_stream.split();
+            let client_read = Arc::new(Mutex::new(read_half));
+            let client_write = Arc::new(Mutex::new(write_half));
 
             // Spawn a task for handling and tracking the new client
             tokio::spawn(async move {
-                let client_id =
-                    Self::handle_new_client(&clients_by_id_clone, &client, &next_client_id_clone)
-                        .await;
-                Self::listen_to_client(&client, &client_id).await;
-                Self::handle_client_disconnected(&clients_by_id_clone, &client_id).await;
+                let client_id = Self::handle_new_client(
+                    &clients_by_id_read,
+                    &clients_by_id_write,
+                    &client_read,
+                    &client_write,
+                    &next_client_id_clone,
+                )
+                .await;
+                Self::listen_to_client(&client_read, &client_id).await;
+                Self::handle_client_disconnected(
+                    &clients_by_id_read,
+                    &clients_by_id_write,
+                    &client_id,
+                )
+                .await;
             });
         }
 
@@ -67,7 +91,7 @@ impl WebSocketHub {
     /// Sends a message to all subscribers.
     pub async fn broadcast_message(&self, message: String) -> Result<(), Error> {
         // TODO: Explore the possibility of a more granular/semantic lock
-        let clients = self.clients_by_id.lock().await;
+        let clients = self.clients_by_id_write.lock().await;
 
         // Send messages to each client in parallel
         let futures: Vec<_> = clients
@@ -93,22 +117,28 @@ impl WebSocketHub {
     }
 
     async fn handle_new_client(
-        clients_by_id: &ClientsById,
-        client: &Client,
+        clients_by_id_read: &ClientsByIdRead,
+        clients_by_id_write: &ClientsByIdWrite,
+        client_read: &ClientRead,
+        client_write: &ClientWrite,
         next_client_id: &Arc<AtomicU32>,
     ) -> u32 {
         let client_id = next_client_id.fetch_add(1, Ordering::Relaxed);
 
         {
-            let mut clients = clients_by_id.lock().await;
-            clients.insert(client_id, client.clone());
+            let mut clients = clients_by_id_read.lock().await;
+            clients.insert(client_id, client_read.clone());
+        }
+        {
+            let mut clients = clients_by_id_write.lock().await;
+            clients.insert(client_id, client_write.clone());
         }
 
         println!("New client: {}", client_id);
         return client_id;
     }
 
-    async fn listen_to_client(client: &Client, client_id: &u32) {
+    async fn listen_to_client(client: &ClientRead, client_id: &u32) {
         // Here we're just looping to detect disconnection.
         while let Some(result) = client.lock().await.next().await {
             match result {
@@ -123,9 +153,20 @@ impl WebSocketHub {
         }
     }
 
-    async fn handle_client_disconnected(clients_by_id: &ClientsById, client_id: &u32) {
-        let mut clients_lock = clients_by_id.lock().await;
-        clients_lock.remove(client_id);
+    async fn handle_client_disconnected(
+        clients_by_id_read: &ClientsByIdRead,
+        clients_by_id_write: &ClientsByIdWrite,
+        client_id: &u32,
+    ) {
+        {
+            let mut clients_lock = clients_by_id_read.lock().await;
+            clients_lock.remove(client_id);
+        }
+        {
+            let mut clients_lock = clients_by_id_write.lock().await;
+            clients_lock.remove(client_id);
+        }
+
         println!("Client {} disconnected", client_id);
     }
 }
