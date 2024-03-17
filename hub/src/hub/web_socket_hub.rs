0ba04d5cfb7a3007cd4b2fe208ee4b6176466f0e
daxtonb <daxtonburgon@gmail.com>
2024-03-17 01:10:31 -0400

    wip

--- hub/src/hub/web_socket_hub.rs
+++ hub/src/hub/web_socket_hub.rs
@@ -14,7 +14,8 @@ use tokio_tungstenite::{
     WebSocketStream,
 };
 
-type ClientsById = Arc<Mutex<HashMap<u32, Arc<Mutex<WebSocketStream<TcpStream>>>>>>;
+type Client = Arc<Mutex<WebSocketStream<TcpStream>>>;
+type ClientsById = Arc<Mutex<HashMap<u32, Client>>>;
 
 /// Hub for managing web socket communication.
 pub struct WebSocketHub {
@@ -48,13 +49,14 @@ impl WebSocketHub {
 
             let clients_by_id_clone = self.clients_by_id.clone();
             let next_client_id_clone = self.next_client_id.clone();
+            let client = Arc::new(Mutex::new(ws_stream));
 
             // Spawn a task for handling and tracking the new client
             tokio::spawn(async move {
                 let client_id =
-                    Self::handle_new_client(&clients_by_id_clone, ws_stream, &next_client_id_clone)
+                    Self::handle_new_client(&clients_by_id_clone, &client, &next_client_id_clone)
                         .await;
-                Self::listen_to_client(&clients_by_id_clone, &client_id).await;
+                Self::listen_to_client(&client, &client_id).await;
                 Self::handle_client_disconnected(&clients_by_id_clone, &client_id).await;
             });
         }
@@ -92,43 +94,32 @@ impl WebSocketHub {
 
     async fn handle_new_client(
         clients_by_id: &ClientsById,
-        ws_stream: WebSocketStream<TcpStream>,
+        client: &Client,
         next_client_id: &Arc<AtomicU32>,
     ) -> u32 {
         let client_id = next_client_id.fetch_add(1, Ordering::Relaxed);
-        let client_arc = Arc::new(Mutex::new(ws_stream));
-        let client_arc_clone = client_arc.clone();
 
         {
             let mut clients = clients_by_id.lock().await;
-            clients.insert(client_id, client_arc_clone);
+            clients.insert(client_id, client.clone());
         }
 
         println!("New client: {}", client_id);
         return client_id;
     }
 
-    async fn listen_to_client(clients_by_id: &ClientsById, client_id: &u32) {
-        let client = clients_by_id.lock().await.get(client_id).cloned();
-        match client {
-            Some(client) => {
-                // Here we're just looping to detect disconnection.
-                while let Some(result) = client.lock().await.next().await {
-                    match result {
-                        Ok(_) => {
-                            // TODO: handle incoming client messages
-                        }
-                        Err(e) => {
-                            eprintln!("Error on WebSocket for client {:?}: {:?}", client_id, e);
-                            break; // Exit gracefully
-                        }
-                    }
+    async fn listen_to_client(client: &Client, client_id: &u32) {
+        // Here we're just looping to detect disconnection.
+        while let Some(result) = client.lock().await.next().await {
+            match result {
+                Ok(_) => {
+                    // TODO: handle incoming client messages
+                }
+                Err(e) => {
+                    eprintln!("Error on WebSocket for client {:?}: {:?}", client_id, e);
+                    break; // Exit gracefully
                 }
             }
-            None => eprintln!(
-                "Something went wrong while trying to listen to client {}",
-                client_id
-            ),
         }
     }
 
@@ -142,12 +133,12 @@ impl WebSocketHub {
 #[cfg(test)]
 mod integration_tests {
     use super::*;
-    use tokio::net::TcpStream;
-    use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
     use futures_util::sink::SinkExt;
     use futures_util::stream::StreamExt;
     use std::time::Duration;
+    use tokio::net::TcpStream;
     use tokio::time::timeout;
+    use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
     #[tokio::test]
     async fn test_client_connect_broadcast_and_disconnect() {
@@ -169,15 +160,16 @@ mod integration_tests {
             .expect("Failed to connect to WebSocketHub");
 
         // Send a message from the client to test broadcasting.
-        // let test_message = "Hello, WebSocketHub!";
-        // ws_stream
-        //     .send(Message::Text(test_message.to_string()))
-        //     .await
-        //     .expect("Failed to send message from client");
+        let test_message = "Hello, WebSocketHub!";
+        ws_stream
+            .send(Message::Text(test_message.to_string()))
+            .await
+            .expect("Failed to send message from client");
 
         // Broadcast a message to all clients, including the one we just connected.
         let broadcast_message = "Broadcast message from hub";
-        hub_clone.broadcast_message(broadcast_message.to_string())
+        hub_clone
+            .broadcast_message(broadcast_message.to_string())
             .await
             .expect("Failed to broadcast message");
 
@@ -195,10 +187,12 @@ mod integration_tests {
         }
 
         // Disconnect the client.
-        ws_stream.close(None).await.expect("Failed to close the WebSocket stream");
+        ws_stream
+            .close(None)
+            .await
+            .expect("Failed to close the WebSocket stream");
 
         // Optionally, ensure the server has processed the disconnection. This may involve
         // additional logic in your server to track disconnections or using sleeps for simplicity.
     }
 }
-
